53b2b4cb1bbad4cec6c5b7b7c2dc40f1ce15f0c6
Joona Kemppainen <joona.kemppainen@pm.me>
2025-04-07 22:06:40 -0400

    add progress charts

--- src/app/measurements/components/MeasurementChart.tsx
+++ src/app/measurements/components/MeasurementChart.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import {
-  LineChart,
+  CartesianGrid,
+  LabelList,
   Line,
+  LineChart,
   XAxis,
   YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
+} from 'recharts';
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+  type ChartConfig,
 } from '@/components/ui/chart';
-import { MeasurementEntry, MetricType, metricConfigs } from '../types';
+import { MeasurementEntry, MetricType } from '../types';
 
 interface MeasurementChartProps {
   entries: MeasurementEntry[];
@@ -20,25 +25,70 @@ export function MeasurementChart({
   entries,
   selectedMetric,
 }: MeasurementChartProps) {
+  const chartConfig = {
+    value: {
+      label: selectedMetric,
+      color: 'hsl(var(--chart-1))',
+    },
+  } satisfies ChartConfig;
+  if (entries.length === 0) {
+    return (
+      <div className='flex items-center justify-center h-[200px] text-gray-500'>
+        No data available
+      </div>
+    );
+  }
   return (
-    <div className='h-[300px]'>
-      <ResponsiveContainer width='100%' height='100%'>
-        <LineChart data={entries}>
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='date' />
-          <YAxis />
-          <Tooltip
-            formatter={(value, name) => {
-              const entry = entries.find((e) => e.metricValue === value);
-              return [
-                `${entry?.value} ${entry?.unit}`,
-                metricConfigs[selectedMetric].label,
-              ];
-            }}
+    <ChartContainer config={chartConfig} className='min-h-[200px] w-full'>
+      <LineChart
+        accessibilityLayer
+        data={entries.toReversed()}
+        margin={{
+          top: 20,
+          right: 20,
+          left: 20,
+          bottom: 20,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey='date'
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          // tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <YAxis
+          tickLine={false}
+          axisLine={false}
+          tickMargin={12}
+          unit={entries[0].unit}
+        />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent indicator='line' />}
+        />
+        <Line
+          dataKey='value'
+          type='linear'
+          stroke='var(--chart-3)'
+          strokeWidth={2}
+          dot={{
+            fill: 'var(--chart-3)',
+          }}
+          activeDot={{
+            r: 6,
+          }}
+        >
+          <LabelList
+            position='top'
+            offset={12}
+            className='fill-foreground'
+            fontSize={12}
+            formatter={(value: number) => value.toFixed(1)}
           />
-          <Line type='monotone' dataKey='metricValue' stroke='#8884d8' />
-        </LineChart>
-      </ResponsiveContainer>
-    </div>
+        </Line>
+      </LineChart>
+    </ChartContainer>
   );
 }
